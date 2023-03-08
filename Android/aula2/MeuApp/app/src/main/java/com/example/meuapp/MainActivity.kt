f7fe2295b0e18e77b46add40cc9164553692fe39
SonExpedito <alexexpeditoff7@gmai.com>
2023-03-08 10:44:20 -0300

    Testando

---
+++ Android/aula2/MeuApp/app/src/main/java/com/example/meuapp/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.meuapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val edtNome:EditText = findViewById(R.id.edtNome)
+        val btnCadastrar:Button = findViewById(R.id.btnCadastrar)
+        btnCadastrar.setOnClickListener{
+            val toast= Toast.makeText(this, edtNome.toString(), Toast.LENGTH_SHORT)
+            toast.show()
+        }
+    }
+}
