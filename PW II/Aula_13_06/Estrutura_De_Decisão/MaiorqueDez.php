07e5c57fb8010b74fd22b7ce3d34946c8d7b2995
SonExpedito <alexexpeditoff7@gmai.com>
2023-03-13 21:14:54 -0300

    Pronto

---
+++ PW II/Aula_13_06/Estrutura_De_Decisão/MaiorqueDez.php
@@ -0,0 +1,15 @@
+<html>
+    <head>
+        <title>Maior que Dez </title>
+    </head>
+    <body>
+        <br>
+        <?php 
+         $A = $_POST['txta'];  
+         $B = $_POST['txtb']; 
+          $soma= $A + $B;
+          echo "A soma dos valores é ".$soma .'<br>';  
+          if ($soma>10) {    echo "Soma maior que dez ";}
+          ?>
+    </body>
+</html> 
