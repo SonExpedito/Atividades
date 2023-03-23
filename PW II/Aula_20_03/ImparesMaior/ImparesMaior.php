88f111c22b643470e28f6ecd7cf33a76f088373b
Expeditozin <117395239+SonExpedito@users.noreply.github.com>
2023-03-23 14:51:53 -0300

    Finalizado

---
+++ PW II/Aula_20_03/ImparesMaior/ImparesMaior.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>ímpares-Entre</title>
+</head>
+<body>
+     <?php
+         $N1 = $_POST['N1'];
+         $N2 = $_POST['N2'];
+         
+
+        if ($N1==$N2){
+             echo "Os Valores são iguais e não possuem valores ímpares entre eles.";
+        }
+        else{
+             if($N1>$N2){
+                  if($N1 %2 != 0){
+                       for($Cont = $N1; $Cont>=$N2; $Cont=$Cont-2){
+                            echo $Cont ."<br>";
+                            
+                       }
+
+                  }
+                  else{
+                    for($Cont = $N1+1; $Cont>=$N2; $Cont=$Cont-2){
+                         echo $Cont ."<br>";
+                         
+                    }
+
+
+                  }
+
+
+             }
+             else{
+                   if($N2 %2 != 0){
+                       for($Cont = $N2; $Cont>=$N1; $Cont=$Cont-2){
+                           echo $Cont ."<br>";
+                         
+                     }
+
+                   }
+                   else{
+                       for($Cont = $N2+1; $Cont>=$N1; $Cont=$Cont-2){
+                      echo $Cont ."<br>";
+                      
+                   }      
+
+
+             }
+ 
+        }
+     }
+      ?>
+</body>
+</html>
